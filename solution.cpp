979dd23619bcc222a3603f3ab35e6cca214e9991
AndyLiming <mingli6250@163.com>
2020-07-06 16:22:57 +0800

    update No 837

--- solution.cpp
+++ solution.cpp
@@ -4697,9 +4697,16 @@ vector<int> solution::prisonAfterNDaysNext(vector<int>& cells)
 	return res;
 }
 
-
 //No 837 New 21 Game
 double solution::new21Game(int N, int K, int W)
 {
-	return 0.0;
+	if (K == 0 || N >= K + W) return 1.0;
+	vector<double>sum(K + W);
+	sum[0] = 1.0;
+	for (int i = 1; i < K + W; ++i) {
+		int t = min(i - 1, K - 1);
+		if (i <= W) sum[i] = sum[i - 1] + sum[t] / W;
+		else sum[i] = sum[i - 1] + (sum[t] - sum[i - W - 1]) / W;
+	}
+	return (sum[N] - sum[K - 1]) / (sum[K + W - 1] - sum[K - 1]);
 }
