92c5e202d4d5928aa3de04f673ff989c2cc959a7
AndyLiming <mingli6250@163.com>
2020-07-30 11:19:26 +0800

    add new solution for No 309

--- solution.cpp
+++ solution.cpp
@@ -2692,6 +2692,18 @@ int solution::maxProfit(vector<int> & prices)
 		sell = max(sellOld, prices[i] - buy);
 	}
 	return sell;
+
+	//if (prices.empty()) return 0;
+	//int f0 = -prices[0],f1=0,f2=0;
+	//for (int i = 1; i < prices.size(); ++i) {
+	//	int newf0 = max(f0, f2 - prices[i]);
+	//	int newf1 = f1 + prices[i];
+	//	int newf2 = max(f1, f2);
+	//	f0 = newf0;
+	//	f1 = newf1;
+	//	f2 = newf2;
+	//}
+	//return max(f1, f2);
 }
 
 //No 327 Count of Range Sum
